8b3cd4fbe56331fdbefe750616e5cc88fdd3f164
Basel Hassan <baselhassan150@gmail.com>
2026-07-27 17:27:11 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,245 @@
+var html = document.documentElement
+var htmlTheme = html.classList
+var body = document.body
+var themeToggleButton = document.getElementById("theme-toggle-button")
+var scrollToTop = document.getElementById("scroll-to-top")
+var heroSection = document.getElementById("hero-section")
+var aboutSection = document.getElementById("about")
+var portfolioSection = document.getElementById("portfolio")
+var experienceSection = document.getElementById("experience")
+var testimonialsSection = document.getElementById("testimonials")
+var contactSection = document.getElementById("contact")
+var navLinksArr = document.querySelectorAll(".nav-links a")
+var portfolioFiltersArr = Array.from(document.querySelectorAll("#portfolio-filters button"))
+var portfolioGridArr = Array.from(document.querySelectorAll("#portfolio-grid > div"))
+var navBar = document.getElementById("header")
+var navPosition = navBar.getBoundingClientRect().bottom
+var settingsToggle = document.getElementById("settings-toggle")
+var settingsSidebar = document.getElementById("settings-sidebar")
+var closeSettings = document.getElementById("close-settings")
+var resetSettings = document.getElementById("reset-settings")
+var fontOptions = Array.from(document.querySelectorAll(".font-option"))
+var checkFont = JSON.parse(localStorage.getItem("ariaChecked"))
+
+
+storeagetheme()
+fontStorage()
+changeFont()
+
+//*  Theame 
+themeToggleButton.addEventListener("click" , function () {
+    html.classList.toggle("dark")
+    htmlTheme = html.classList
+    localStorage.setItem("htmlTheme" , JSON.stringify(htmlTheme.value))
+})
+
+function storeagetheme() {
+    if (JSON.parse(localStorage.getItem("htmlTheme")) == "dark") {
+        html.classList.add("dark")
+    }else if (JSON.parse(localStorage.getItem("htmlTheme")) != "dark" && JSON.parse(localStorage.getItem("htmlTheme")) != null ) {
+        html.classList.remove("dark")
+    }
+}
+
+//*  Scroll To Top
+document.addEventListener("scroll" , function () {
+    
+    if (window.scrollY >= aboutSection.offsetTop) {
+        scrollToTop.classList.add("show-btn")
+    }else {
+        scrollToTop.classList.remove("show-btn")
+    }
+})
+scrollToTop.addEventListener("click" , function () {
+    scroll(0,0)
+})
+
+//*  Scroll To Spy
+function activeScrollSpy() {
+        if (navPosition >= heroSection.getBoundingClientRect().top && navPosition <= heroSection.getBoundingClientRect().bottom) {
+            navLinksArr[0].classList.add("active")
+        }else {
+            navLinksArr[0].classList.remove("active")
+        }
+
+        if (navPosition >= aboutSection.getBoundingClientRect().top && navPosition <= aboutSection.getBoundingClientRect().bottom) {
+            navLinksArr[1].classList.add("active")
+        }else {
+            navLinksArr[1].classList.remove("active")
+        }
+
+        if (navPosition >= portfolioSection.getBoundingClientRect().top && navPosition <= portfolioSection.getBoundingClientRect().bottom) {
+            navLinksArr[2].classList.add("active")
+        }else {
+            navLinksArr[2].classList.remove("active")
+        }
+
+        if (navPosition >= experienceSection.getBoundingClientRect().top && navPosition <= experienceSection.getBoundingClientRect().bottom) {
+            navLinksArr[3].classList.add("active")
+        }else {
+            navLinksArr[3].classList.remove("active")
+        }
+
+        if (navPosition >= testimonialsSection.getBoundingClientRect().top && navPosition <= testimonialsSection.getBoundingClientRect().bottom) {
+            navLinksArr[4].classList.add("active")
+        }else {
+            navLinksArr[4].classList.remove("active")
+        }
+
+        if (navPosition >= contactSection.getBoundingClientRect().top && navPosition <= contactSection.getBoundingClientRect().bottom) {
+            navLinksArr[5].classList.add("active")
+        }else {
+            navLinksArr[5].classList.remove("active")
+        }
+    }
+
+document.addEventListener("scroll" , activeScrollSpy)
+
+//*  Navs & Tabs
+for (let i = 0; i < portfolioFiltersArr.length; i++) {
+        portfolioFiltersArr[i].addEventListener("click" , function (e) {
+        e.target.classList.add("bg-linear-to-r")
+        e.target.classList.add("from-primary")
+        e.target.classList.add("to-secondary")
+        e.target.classList.add("text-white")
+        e.target.classList.add("hover:shadow-lg")
+        e.target.classList.add("hover:shadow-primary/50")
+        e.target.classList.remove("bg-white")
+        e.target.classList.remove("dark:bg-slate-800")
+        e.target.classList.remove("text-slate-600")
+        e.target.classList.remove("dark:text-slate-300")
+        e.target.classList.remove("hover:bg-slate-100")
+        e.target.classList.remove("dark:hover:bg-slate-700")
+        e.target.classList.remove("border")
+        e.target.classList.remove("border-slate-300")
+        e.target.classList.remove("dark:border-slate-700")
+        
+        for (let index = 0; index < portfolioGridArr.length; index++) {
+            if (e.target.getAttribute("data-filter") == "all") {
+                portfolioGridArr[index].style.display = "";
+                setTimeout(() => {
+                    portfolioGridArr[index].classList.add("card-on")
+                    portfolioGridArr[index].classList.remove("card-off")
+                });
+            }else if(e.target.getAttribute("data-filter") !== portfolioGridArr[index].getAttribute("data-category")) {
+                portfolioGridArr[index].classList.remove("card-on")
+                portfolioGridArr[index].classList.add("card-off")
+                setTimeout(() => {
+                                    portfolioGridArr[index].style.display = "none";
+                }, 300);
+            } else {
+                portfolioGridArr[index].style.display = "";    
+                                setTimeout(() => {
+                    portfolioGridArr[index].classList.add("card-on")
+                    portfolioGridArr[index].classList.remove("card-off")
+                });  
+                }
+        }
+
+
+        for (let x = 0; x < portfolioFiltersArr.length; x++) {
+            if (portfolioFiltersArr[x] !== e.target) {
+                portfolioFiltersArr[x].classList.remove("bg-linear-to-r")
+                portfolioFiltersArr[x].classList.remove("from-primary")
+                portfolioFiltersArr[x].classList.remove("to-secondary")
+                portfolioFiltersArr[x].classList.remove("text-white")
+                portfolioFiltersArr[x].classList.remove("hover:shadow-lg")
+                portfolioFiltersArr[x].classList.remove("hover:shadow-primary/50")
+                portfolioFiltersArr[x].classList.add("bg-white")
+                portfolioFiltersArr[x].classList.add("dark:bg-slate-800")
+                portfolioFiltersArr[x].classList.add("text-slate-600")
+                portfolioFiltersArr[x].classList.add("dark:text-slate-300")
+                portfolioFiltersArr[x].classList.add("hover:bg-slate-100")
+                portfolioFiltersArr[x].classList.add("dark:hover:bg-slate-700")
+                portfolioFiltersArr[x].classList.add("border")
+                portfolioFiltersArr[x].classList.add("border-slate-300")
+                portfolioFiltersArr[x].classList.add("dark:border-slate-700")
+            }
+            
+        }
+        
+    })
+}
+
+//*  Gear Side Bar
+
+settingsToggle.addEventListener("click" , function() {
+    settingsSidebar.classList.remove("translate-x-full")
+    settingsToggle.style.right = "20rem"
+})
+
+closeSettings.addEventListener("click" , function() {
+    settingsSidebar.classList.add("translate-x-full")
+    settingsToggle.style.right = ""
+})
+
+function fontActivate(option) {
+    option.classList.remove("border-slate-200", "dark:border-slate-700");
+    option.classList.add(
+        "active",
+        "border-primary",
+        "bg-slate-50",
+        "dark:bg-slate-800"
+    );
+}
+
+function notFontActivate(option) {
+    option.classList.add("border-slate-200", "dark:border-slate-700");
+    option.classList.remove(
+        "active",
+        "border-primary",
+        "bg-slate-50",
+        "dark:bg-slate-800"
+    );
+}
+
+function changeFont() {
+    for (let i = 0; i < fontOptions.length; i++) {
+        
+        if (fontOptions[i].getAttribute("aria-checked") == "true") {
+            fontActivate(fontOptions[i])
+            body.classList.add(`font-${fontOptions[i].getAttribute("data-font")}`)
+        }else {
+            body.classList.remove(`font-${fontOptions[i].getAttribute("data-font")}`)
+            notFontActivate(fontOptions[i])
+        }
+
+        fontOptions[i].addEventListener("click" , function() {
+            fontOptions[i].setAttribute("aria-checked", "true")
+            for (let index = 0; index < fontOptions.length; index++) {
+                body.classList.remove(`font-${fontOptions[index].getAttribute("data-font")}`)
+                if (index !== i) {
+                    notFontActivate(fontOptions[index])
+                    fontOptions[index].setAttribute("aria-checked", "false")
+                }
+            }
+
+            body.classList.add(`font-${fontOptions[i].getAttribute("data-font")}`)
+            fontActivate(fontOptions[i])
+            localStorage.setItem("ariaChecked" , i)
+        })
+    }
+}
+
+function fontStorage() {
+    if (checkFont != null) {
+        for (let i = 0; i < fontOptions.length; i++) {
+            fontOptions[i].setAttribute("aria-checked", "false")
+        }
+        fontOptions[checkFont].setAttribute("aria-checked", "true")
+    }
+}
+
+resetSettings.addEventListener("click" , function () {
+    for (let i = 0; i < fontOptions.length; i++) {
+        if (fontOptions[i].getAttribute("font-position")) {
+            body.classList.add(`font-${fontOptions[i].getAttribute("data-font")}`)
+            fontActivate(fontOptions[i])
+            localStorage.setItem("ariaChecked" , i)
+        }else {
+            body.classList.remove(`font-${fontOptions[i].getAttribute("data-font")}`)
+            notFontActivate(fontOptions[i])
+        }
+    }
+})
+
